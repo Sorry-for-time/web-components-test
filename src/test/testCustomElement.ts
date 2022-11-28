5c768ae722603546baaa078d6c77209377657a01
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-28 19:34:04 +0800

    添加自定义卡片的日志测试

--- src/test/testCustomElement.ts
+++ src/test/testCustomElement.ts
@@ -6,6 +6,10 @@ export async function testCustomElement() {
   const customCards: Array<CustomCard> = Array.from(
     document.querySelectorAll("custom-card")
   );
+
+  customCards.forEach((e) => {
+    console.log("创建耗时: ", e.createCostTime);
+  });
   for await (const v of iterableConstructorBackItemByDelay(customCards)) {
     dragView.removeChild(v as HTMLElement);
   }
