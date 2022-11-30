e2b2202c8f03195c7d2c476c3b23cf3dfb0c506d
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-30 22:41:56 +0800

    添加一个简单的 Dialog 组件

---
+++ src/lib/components/Dialog.ts
@@ -0,0 +1,81 @@
+import { WebComponentBase } from "../WebComponentBase.js";
+
+export class Dialog extends WebComponentBase {
+  private container: HTMLDivElement | null = null;
+  private confirmButton: HTMLButtonElement | null;
+  private cancelButton: HTMLButtonElement | null;
+  private titleEl: HTMLParagraphElement | null = null;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" }).appendChild(
+      (
+        document.querySelector("#dialog") as HTMLTemplateElement
+      ).content.cloneNode(true)
+    );
+
+    this.container = this.shadowRoot?.querySelector(".__container")!;
+    this.titleEl = this.shadowRoot?.querySelector("h1")!;
+    this.confirmButton = this.shadowRoot?.querySelector("#confirm")!;
+    this.cancelButton = this.shadowRoot?.querySelector("#cancel")!;
+  }
+
+  disconnectedCallback(): void {
+    this.container!.onclick = null;
+  }
+
+  /**
+   * 弹窗处处理函数
+   * @param ev 鼠标点击事件
+   * @param handler
+   * @returns
+   */
+  private handler = async (
+    ev: MouseEvent,
+    handler: (value: boolean) => void
+  ) => {
+    return new Promise((resolve, reject) => {
+      let value: boolean = false;
+      switch (ev.target) {
+        case this.confirmButton:
+          value = true;
+          break;
+        case this.cancelButton:
+          value = false;
+          break;
+        default:
+          break;
+      }
+      this.container?.classList.add("hide");
+      if (value) {
+        resolve(true);
+        this.container!.onclick = null;
+      } else {
+        reject(false);
+      }
+    })
+      .then(async (success) => {
+        if (typeof handler === "function") {
+          handler(success as boolean);
+        }
+      })
+      .catch((reason) => {
+        if (typeof handler === "function") {
+          handler(reason);
+        }
+      });
+  };
+
+  /**
+   * 调用弹窗函
+   * @param title 对话框标题
+   * @param handler  处理函数
+   */
+  public alert(title: string = "", handler: (value: boolean) => void): void {
+    this.titleEl!.innerText = title;
+    this.container?.classList.remove("hide");
+    this.container!.onclick = (ev: MouseEvent): void => {
+      this.handler(ev, handler);
+    };
+  }
+}
