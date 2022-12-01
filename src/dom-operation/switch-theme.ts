7e93a829dbc7b54f090cb7ceaf0d9ec4d07778e6
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-12-01 13:16:42 +0800

    添加主题, 以及配色上的微调整

--- src/dom-operation/switch-theme.ts
+++ src/dom-operation/switch-theme.ts
@@ -5,6 +5,7 @@ const enum THEMES {
   "normal" = "normal-theme",
   "dark" = "dark-theme",
   "special" = "special-theme",
+  "gray" = "gray-theme",
 }
 
 /**
@@ -33,6 +34,9 @@ export function useSwitchTheme(): void {
       case THEMES.special:
         themeIDTag = THEMES.special;
         break;
+      case THEMES.gray:
+        themeIDTag = THEMES.gray;
+        break;
       default:
         themeIDTag = THEMES.normal;
         break;
@@ -49,7 +53,7 @@ export function useSwitchTheme(): void {
   else {
     /* 判断当前系统默认颜色是否处于暗色模式 */
     const colorMode: boolean = window.matchMedia(
-      "(prefers-color-scheme: dark)"
+      "(prefers-color-scheme: dark)",
     ).matches;
     if (colorMode) {
       document.body.classList.add(THEMES.dark);
@@ -81,9 +85,9 @@ export function useSwitchTheme(): void {
 
       /* 清空所有主题 */
       document.body.classList.remove(
-        ...document.body.classList.toString().split(" ")
+        ...document.body.classList.toString().split(" "),
       );
-      let spanThemeStr: string = "";
+      let spanThemeStr: string = THEMES.normal;
       switch (span.id) {
         case THEMES.normal:
           spanThemeStr = THEMES.normal;
@@ -94,10 +98,14 @@ export function useSwitchTheme(): void {
         case THEMES.special:
           spanThemeStr = THEMES.special;
           break;
+        case THEMES.gray:
+          spanThemeStr = THEMES.gray;
+          break;
         default:
           /* 除非非法修改, 不然应该不会有什么问题 */
           break;
       }
+
       document.body.classList.add(spanThemeStr);
       span.classList.add("active");
       localStorage.setItem("theme-mode", spanThemeStr);
