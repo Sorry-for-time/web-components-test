3a8e9bbcdda0857bb0233a417d38065992933a20
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-12-06 18:17:40 +0800

    添加节流包装工具函数

--- src/utils/performanceUtil.ts
+++ src/utils/performanceUtil.ts
@@ -1,4 +1,5 @@
 type DEBOUNCE_INSTANCE = () => void;
+type THROTTLE_INSTANCE = () => void;
 
 /**
  * 防抖包装工具函数
@@ -6,7 +7,7 @@ type DEBOUNCE_INSTANCE = () => void;
  * @param startImmediately 是否在第一次调用函数的时候立即执行, 无需等待, 默认为 true
  * @param delay 执行延迟, 默认 200ms
  * @param params 函数的参数
- * @returns 防抖包装结果
+ * @returns 防抖包装函数
  */
 export const debounce = (
   executorFn: Function,
@@ -24,6 +25,42 @@ export const debounce = (
         clearTimeout(timer);
       }
       timer = setTimeout(executorFn, delay, ...params);
+    }
+  };
+};
+
+/**
+ * 自定义节流包装函数
+ * @param executorFn  要进行节流的函数
+ * @param startImmediately 首次是否立即指向
+ * @param delay 延迟
+ * @param params 函数的参数
+ * @returns 节流包装函数
+ */
+export const throttle = (
+  executorFn: Function,
+  startImmediately: boolean = true,
+  delay: number = 200,
+  ...params: Array<any>
+): THROTTLE_INSTANCE => {
+  let locker: boolean = false;
+  return () => {
+    if (startImmediately) {
+      startImmediately = false;
+      locker = true;
+      setTimeout((): void => {
+        executorFn(...params);
+        locker = false;
+      });
+    } else {
+      if (locker) {
+        return;
+      }
+      locker = true;
+      setTimeout((): void => {
+        executorFn(...params);
+        locker = false;
+      }, delay);
     }
   };
 };
