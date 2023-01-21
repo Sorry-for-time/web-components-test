83289b5fb74e38164b0fc6a4683d7662fad3f182
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-22 02:40:09 +0800

    添加打包结果类别占比分析插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import autoprefixer from "autoprefixer";
 import { fileURLToPath, URL } from "node:url";
+import { visualizer } from "rollup-plugin-visualizer";
 import { ConfigEnv, defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,7 +8,12 @@ export default defineConfig(({ mode }: ConfigEnv): any => {
   const env: Record<string, string> = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [],
+    plugins: [
+      // 编译结果分析插件
+      visualizer({
+        open: true
+      })
+    ],
     server: {
       port: Number.parseInt(env.VITE_SERVER_PORT),
       open: env.VITE_OPEN_BROWSER === "true" ? true : false
